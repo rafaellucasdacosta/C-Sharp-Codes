98823243949cdebc8bad316cf6898761d011346e
Rafael <rafaellucasdacosta42@gmail.com>
2020-12-01 12:13:06 -0300

    Working with Files

---
+++ Files/Files/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+    }
+}
