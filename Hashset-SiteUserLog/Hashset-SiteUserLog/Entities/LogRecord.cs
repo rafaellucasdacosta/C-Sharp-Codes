b9bd8fe51bec1f42084eed666fbaf9c14f03a580
Rafael <rafaellucasdacosta42@gmail.com>
2020-12-03 16:19:12 -0300

    Using hashset and equals

---
+++ Hashset-SiteUserLog/Hashset-SiteUserLog/Entities/LogRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Hashset_SiteUserLog.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string username, DateTime instant)
+        {
+            Username = username;
+            Instant = instant;
+        }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ Hashset-SiteUserLog/Hashset-SiteUserLog/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Hashset_SiteUserLog.Entities;
+using System.Collections.Generic;
+
+namespace Hashset_SiteUserLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ') ;
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord(name, instant));
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
